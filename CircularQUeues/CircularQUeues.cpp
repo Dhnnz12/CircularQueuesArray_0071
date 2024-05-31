4c673649e53426300336cd5035170e1e02f6ef89
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-05-31 14:44:26 +0700

    menambahkan else dan while

--- CircularQUeues/CircularQUeues.cpp
+++ CircularQUeues/CircularQUeues.cpp
@@ -71,4 +71,24 @@ void display() {
 			FRONT_position++;
 		}
 		cout << endl;
+	}
+	else
+	{
+		while (FRONT_position <= max - 1)
+		{
+			cout << queue_array[FRONT_position] << "  ";
+			FRONT_position++;
+		}
+		FRONT_position = 0;
+
+		while (FRONT_position <= REAR_position)
+		{
+			cout << queue_array[FRONT_position] << "  ";
+			FRONT_position++;
+		}
+		cout << endl;
+	}
+}
+
+};
 
